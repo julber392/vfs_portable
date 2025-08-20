927491902671b18db3981d710313c8c98ea36755
julber392 <supermatrix39@gmail.com>
2025-08-20 22:29:38 +0300

    init

--- src/vfs.cpp
+++ src/vfs.cpp
@@ -5,3 +5,7 @@
 #include <filesystem>
 
 using namespace std;
+
+VirtualFileSystem::VirtualFileSystem(const std::string &path, size_t blocks, size_t blockSize) :
+        diskPath(path), totalBlocks(blocks), blockSize(blockSize) {};
+
