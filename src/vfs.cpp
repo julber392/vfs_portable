8d9bc98356242bcfb12d4dcb6f8b2f0a256eda7b
julber392 <supermatrix39@gmail.com>
2025-08-22 22:17:07 +0300

    mkfs

--- src/vfs.cpp
+++ src/vfs.cpp
@@ -9,3 +9,13 @@ using namespace std;
 VirtualFileSystem::VirtualFileSystem(const std::string &path, size_t blocks, size_t blockSize) :
         diskPath(path), totalBlocks(blocks), blockSize(blockSize) {};
 
+bool VirtualFileSystem::mkfs() {
+    fstream file(diskPath, ios::binary | ios::out | ios::trunc);
+    if (!file) return false;
+
+    vector<char> zero(blockSize, 0);
+    for (size_t i = 0; i < totalBlocks; i++) {
+        file.write(zero.data(), blockSize);
+    }
+    return true;
+}
